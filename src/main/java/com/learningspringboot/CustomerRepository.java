fc2125074b596bc8d9e51c9800608ea709fd923e
Damian <damian.makoza@gmail.com>
2023-02-23 11:27:37 +0100

    Created api v1

---
+++ src/main/java/com/learningspringboot/CustomerRepository.java
@@ -0,0 +1,7 @@
+package com.learningspringboot;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CustomerRepository extends JpaRepository<Customer, Integer> {
+
+}
